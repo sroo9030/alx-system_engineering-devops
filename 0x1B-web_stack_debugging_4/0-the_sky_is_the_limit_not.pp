f2100e6d39b4cfaaca226933bb9e4e6d7cd1b1e0
lenovo <eisra3000ese@gmail.com>
2024-08-22 20:39:39 +0300

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,5 @@
 # 0-fix_nginx.pp
 # This Puppet manifest fixes Nginx configuration to handle high load.
 
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/nginx.conf'],
-}
-
 exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
 -> exec { '/usr/bin/env service nginx restart': }
