a9b454bec17d15dc43b6aea79819f2ad8fce876f
lenovo <eisra3000ese@gmail.com>
2024-08-22 20:17:33 +0300

    0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,29 @@
+# 0-fix_nginx.pp
+# This Puppet manifest fixes Nginx configuration to handle high load.
+
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  content => template('nginx/nginx.conf.erb'),  # Assuming you've set up a template
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/nginx.conf'],
+}
+
+exec { 'increase system limits':
+  command => 'echo "* soft nofile 65536" >> /etc/security/limits.conf && echo "* hard nofile 65536" >> /etc/security/limits.conf',
+  unless  => 'grep -q "65536" /etc/security/limits.conf',
+}
+
+exec { 'increase somaxconn':
+  command => '/sbin/sysctl -w net.core.somaxconn=65535',
+  unless  => '/sbin/sysctl -a | grep "net.core.somaxconn = 65535"',
+}
+
+exec { 'increase tcp_max_syn_backlog':
+  command => '/sbin/sysctl -w net.ipv4.tcp_max_syn_backlog=4096',
+  unless  => '/sbin/sysctl -a | grep "net.ipv4.tcp_max_syn_backlog = 4096"',
+}
