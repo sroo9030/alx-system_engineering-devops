40fc61ceb4a2a8a0f7ac55121c1d793d481cfcd5
lenovo <eisra3000ese@gmail.com>
2024-08-22 20:35:46 +0300

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -13,17 +13,5 @@ service { 'nginx':
   subscribe => File['/etc/nginx/nginx.conf'],
 }
 
-exec { 'increase system limits':
-  command => 'echo "* soft nofile 65536" >> /etc/security/limits.conf && echo "* hard nofile 65536" >> /etc/security/limits.conf',
-  unless  => 'grep -q "65536" /etc/security/limits.conf',
-}
-
-exec { 'increase somaxconn':
-  command => '/sbin/sysctl -w net.core.somaxconn=65535',
-  unless  => '/sbin/sysctl -a | grep "net.core.somaxconn = 65535"',
-}
-
-exec { 'increase tcp_max_syn_backlog':
-  command => '/sbin/sysctl -w net.ipv4.tcp_max_syn_backlog=4096',
-  unless  => '/sbin/sysctl -a | grep "net.ipv4.tcp_max_syn_backlog = 4096"',
-}
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
