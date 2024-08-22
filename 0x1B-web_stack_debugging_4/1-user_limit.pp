607238e34bcbc6b3fa29a93121646a0e0e87eb3d
lenovo <eisra3000ese@gmail.com>
2024-08-22 20:48:33 +0300

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,31 +1,3 @@
 # This Puppet manifest ensures the holberton user can log 
 
-user { 'holberton':
-  ensure     => present,
-  managehome => true,
-  password   => 'your_encrypted_password_here', 
-}
-
-group { 'sudo':
-  ensure => present,
-}
-
-user { 'holberton':
-  groups => ['sudo', 'www-data'],
-}
-
-file { '/path/to/file':
-  ensure  => file,
-  owner   => 'holberton',
-  group   => 'holberton',
-  mode    => '0644',
-}
-
-file { '/path/to/directory':
-  ensure  => directory,
-  owner   => 'holberton',
-  group   => 'holberton',
-  mode    => '0755',
-}
-
 exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
